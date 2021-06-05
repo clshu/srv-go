9bb5644b24d74994019e677b54020efa3b9f1f02
clshu <clshu2020@gmail.com>
2021-06-04 17:53:54 -0700

    Add Query.users

--- graph/resolver/user.resolvers.go
+++ graph/resolver/user.resolvers.go
@@ -6,9 +6,11 @@ package resolver
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/clshu/srv-go/graph/model"
 	"github.com/kamva/mgm/v3"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (r *mutationResolver) CreateUser(ctx context.Context, data model.CreateUserInput) (*model.UserView, error) {
@@ -26,7 +28,39 @@ func (r *mutationResolver) LogIn(ctx context.Context, data model.LogInInput) (*m
 }
 
 func (r *queryResolver) Users(ctx context.Context) ([]*model.UserView, error) {
-	panic(fmt.Errorf("not implemented"))
+	user := &model.User{}
+	var results []*model.UserView
+
+	// mctx := mgm.Ctx()
+	cur, err := mgm.Coll(user).Find(context.TODO(), bson.M{})
+	defer (func() {
+		err := cur.Close(context.TODO())
+		if err != nil {
+			log.Fatal(err)
+		}
+	})()
+
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	for cur.Next(context.TODO()) {
+		var user model.User
+		err := cur.Decode(&user)
+		if err != nil {
+			log.Fatal(err)
+			return nil, err
+		}
+		userView := User2UserView(&user)
+		results = append(results, userView)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return results, nil
 }
 
 func (r *queryResolver) Profile(ctx context.Context) (*model.UserView, error) {
